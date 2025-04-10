8bf56fc9fc8b63c14ab82a6cf3bbc0f22b157f7e
dadada96 <saifullah.hamid@Ahmads-MacBook-Air.local>
2025-04-10 16:55:08 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p
+          <div
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
@@ -24,7 +24,7 @@ export default function Page() {
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
-          </p>
+          </div>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
